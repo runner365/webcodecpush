1f4cebacf2662e611306d73ef46ea5be7f597ce3
shiwei <xiaoq_bj@126.com>
2022-02-05 22:00:22 +0800

    update init

--- jssrc/index.js
+++ jssrc/index.js
@@ -1,14 +1,22 @@
 const WsWriter  = require('./WsWriter');
 
+function windowInit() {
+    document.getElementById('serverHost').value = "localhost:1900";
+    document.getElementById('subpath').value = "live/livestream";
+}
 
 async function publish() {
-    let videoElement = document.getElementById('video_container_publish')
+    let videoElement = document.getElementById('video_container_publish');
+    let serverHost = document.getElementById('serverHost').value;
+    let subpath = document.getElementById('subpath').value;
     let wsClient = new WsWriter();
     wsClient.SetVideoElement(videoElement);
-    wsClient.Init("127.0.0.1:1900", "live/6699.flv");
+    console.log("websocket url:", "ws://" + serverHost + "/" + subpath + ".flv")
+    wsClient.Init(serverHost, subpath + ".flv");
 }
 
 document.getElementById('publishId').addEventListener('click', ()=>{
     publish()
 })
 
+windowInit();
